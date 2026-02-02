27c2f37e0d727312dd778a89a6592ab87c08ddd7
eevbb <45163033+eevbb@users.noreply.github.com>
2026-02-02 03:49:30 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ fi
 mkdir -p $dir/MG/{Binaries/Win64/,Content/Paks/~mods}
 
 cp -r ue4ss $dir/MG/Binaries/Win64
-cp MG/Build/Windows/MG/Content/Paks/pakchunk1-Windows.pak $dir/MG/Content/Paks/~mods/Uncensor_P.pak
-cp MG/Build/Windows/MG/Content/Paks/pakchunk1-Windows.utoc $dir/MG/Content/Paks/~mods/Uncensor_P.utoc
-cp MG/Build/Windows/MG/Content/Paks/pakchunk1-Windows.ucas $dir/MG/Content/Paks/~mods/Uncensor_P.ucas
+cp MG/Build/Windows/MG/Content/Paks/pakchunk1-Windows.pak $dir/MG/Content/Paks/~mods/MGFix_P.pak
+cp MG/Build/Windows/MG/Content/Paks/pakchunk1-Windows.utoc $dir/MG/Content/Paks/~mods/MGFix_P.utoc
+cp MG/Build/Windows/MG/Content/Paks/pakchunk1-Windows.ucas $dir/MG/Content/Paks/~mods/MGFix_P.ucas
